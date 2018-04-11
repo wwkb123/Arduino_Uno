695cc2251682d37adbcfea4bf7ab3ac86d2b87d9
Yiu Chung Yau (Tommy) <wwkb1233@gmail.com>
2018-04-11 02:32:38 -0400

    updates

---
+++ sketch_apr11a.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+
+Servo servoRight;
+Servo servoLeft;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  
+  servoRight.attach(12); 
+  servoRight.writeMicroseconds(1300); //right wheel clockwise
+  servoLeft.attach(13);
+  servoLeft.writeMicroseconds(1700); //left wheel anticlockwise
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
